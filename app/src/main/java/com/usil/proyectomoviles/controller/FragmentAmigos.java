4745c8eca94004b46e3e0bec1e90f40d71e2445d
AngelloCruzYarleque <angellocruz0911@gmail.com>
2022-06-10 11:52:01 -0500

    enlace de Fragments con sus layouts

--- app/src/main/java/com/usil/proyectomoviles/controller/FragmentAmigos.java
+++ app/src/main/java/com/usil/proyectomoviles/controller/FragmentAmigos.java
@@ -1,6 +1,26 @@
 package com.usil.proyectomoviles.controller;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.usil.proyectomoviles.R;
+
 public class FragmentAmigos extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.lyt_fgt_amigos,null);
+        return v;
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 }
--- app/src/main/java/com/usil/proyectomoviles/controller/FragmentGrupo.java
+++ app/src/main/java/com/usil/proyectomoviles/controller/FragmentGrupo.java
@@ -1,6 +1,31 @@
 package com.usil.proyectomoviles.controller;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.usil.proyectomoviles.R;
+import com.usil.proyectomoviles.entity.Usuario;
+import com.usil.proyectomoviles.modelo.DAOUsuario;
+
 public class FragmentGrupo extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.lyt_fgt_grupo,null);
+        return v;
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 }
